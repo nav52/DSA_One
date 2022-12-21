61eb9c16f60c7ef0d799e74df5cab3fc7424f358
Naveen Karanth <naveenfaster@gmail.com>
2022-12-21 10:55:14 +0530

    Commit on 21-Dec-2022

---
+++ Sort_Array_By_Parity.py
@@ -0,0 +1,44 @@
+"""
+Question
+
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+"""
+#Solution
+
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # 2 pointer technique
+        run = 0
+        walk = 0
+        while run < len(nums):
+            # if the number is odd, increment the forward pointer
+            if nums[run]%2 != 0:
+                run += 1
+            # if the number is even, swap the number in the pointers to put the odd numbers to the back of the grid
+            else:
+                nums[walk], nums[run] = nums[run], nums[walk]
+                run += 1
+                walk += 1
+        return nums
